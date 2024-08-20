433f10316cf4ba1d77e66dc361d9f1be42412f64
exuanbo <exuanbo@protonmail.com>
2024-08-20 22:18:53 +0800

    test: use namespace import

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,8 +1,10 @@
 import { describe, expect, expectTypeOf, it } from "vitest";
 
-import { entries, forEach, isKey, isValue, key, keys, size, value, values } from "../src";
+import * as E from "../src";
 import { getKeys, getKeysByValue, getValuesByKey } from "../src/metadata";
 import type {
+  AnyEnumObject,
+  AnyEnumValue,
   EnumIteratee,
   EnumObject,
   EnumValueBase,
@@ -52,21 +54,21 @@ describe("Metadata", () => {
 
 describe("Size", () => {
   it("should get size", () => {
-    expect(size(HetEnum)).toBe(4);
+    expect(E.size(HetEnum)).toBe(4);
   });
 });
 
 describe("Iterators", () => {
   it("should get keys", () => {
-    expect([...keys(HetEnum)]).toEqual(["A", "B", "C", "D"]);
+    expect([...E.keys(HetEnum)]).toEqual(["A", "B", "C", "D"]);
   });
 
   it("should get values", () => {
-    expect([...values(HetEnum)]).toEqual([0, "B", 0.1, "E"]);
+    expect([...E.values(HetEnum)]).toEqual([0, "B", 0.1, "E"]);
   });
 
   it("should get entries", () => {
-    expect([...entries(HetEnum)]).toEqual([
+    expect([...E.entries(HetEnum)]).toEqual([
       ["A", 0],
       ["B", "B"],
       ["C", 0.1],
@@ -77,49 +79,49 @@ describe("Iterators", () => {
 
 describe("Getters", () => {
   it("should get value", () => {
-    expect(value(HetEnum, "A")).toBe(0);
-    expect(value(HetEnum, "B")).toBe("B");
-    expect(value(HetEnum, "C")).toBe(0.1);
-    expect(value(HetEnum, "D")).toBe("E");
+    expect(E.value(HetEnum, "A")).toBe(0);
+    expect(E.value(HetEnum, "B")).toBe("B");
+    expect(E.value(HetEnum, "C")).toBe(0.1);
+    expect(E.value(HetEnum, "D")).toBe("E");
 
-    expect(value(HetEnum, "E")).toBeUndefined();
-    expect(value(HetEnum, null)).toBeUndefined();
-    expect(value(HetEnum, undefined)).toBeUndefined();
+    expect(E.value(HetEnum, "E")).toBeUndefined();
+    expect(E.value(HetEnum, null)).toBeUndefined();
+    expect(E.value(HetEnum, undefined)).toBeUndefined();
 
-    expectTypeOf(value(HetEnum, "A")).toEqualTypeOf<Optional<HetEnum>>();
+    expectTypeOf(E.value(HetEnum, "A")).toEqualTypeOf<Optional<HetEnum>>();
   });
 
   it("should get key", () => {
-    expect(key(HetEnum, 0)).toBe("A");
-    expect(key(HetEnum, "B")).toBe("B");
-    expect(key(HetEnum, 0.1)).toBe("C");
-    expect(key(HetEnum, "E")).toBe("D");
+    expect(E.key(HetEnum, 0)).toBe("A");
+    expect(E.key(HetEnum, "B")).toBe("B");
+    expect(E.key(HetEnum, 0.1)).toBe("C");
+    expect(E.key(HetEnum, "E")).toBe("D");
 
-    expect(key(HetEnum, "F")).toBeUndefined();
-    expect(key(HetEnum, null)).toBeUndefined();
-    expect(key(HetEnum, undefined)).toBeUndefined();
+    expect(E.key(HetEnum, "F")).toBeUndefined();
+    expect(E.key(HetEnum, null)).toBeUndefined();
+    expect(E.key(HetEnum, undefined)).toBeUndefined();
 
-    expectTypeOf(key(HetEnum, 0)).toEqualTypeOf<Optional<keyof typeof HetEnum>>();
+    expectTypeOf(E.key(HetEnum, 0)).toEqualTypeOf<Optional<keyof typeof HetEnum>>();
   });
 
   it("should type check value argument", () => {
-    expect(key(NumberEnum, 1)).toBe("A");
+    expect(E.key(NumberEnum, 1)).toBe("A");
     // @ts-expect-error: Argument of type '"A"' is not assignable to parameter of type 'Nullable<number>'
-    expect(key(NumberEnum, "A")).toBeUndefined();
+    expect(E.key(NumberEnum, "A")).toBeUndefined();
 
-    expect(key(StringEnum, "A")).toBe("A");
+    expect(E.key(StringEnum, "A")).toBe("A");
     // @ts-expect-error: Argument of type '0' is not assignable to parameter of type 'Nullable<string>'
-    expect(key(StringEnum, 0)).toBeUndefined();
+    expect(E.key(StringEnum, 0)).toBeUndefined();
 
-    expect(key(HetEnum, 0)).toBe("A");
-    expect(key(HetEnum, "B")).toBe("B");
+    expect(E.key(HetEnum, 0)).toBe("A");
+    expect(E.key(HetEnum, "B")).toBe("B");
     // @ts-expect-error: Argument of type 'true' is not assignable to parameter of type 'Nullable<string | number>'
-    expect(key(HetEnum, true)).toBeUndefined();
+    expect(E.key(HetEnum, true)).toBeUndefined();
   });
 
   describe("Curried", () => {
     it("should get value", () => {
-      const getEnumValue = value(HetEnum);
+      const getEnumValue = E.value(HetEnum);
 
       expect(getEnumValue("A")).toBe(0);
       expect(getEnumValue("B")).toBe("B");
@@ -135,7 +137,7 @@ describe("Getters", () => {
     });
 
     it("should get key", () => {
-      const getEnumKey = key(HetEnum);
+      const getEnumKey = E.key(HetEnum);
 
       expect(getEnumKey(0)).toBe("A");
       expect(getEnumKey("B")).toBe("B");
@@ -151,63 +153,63 @@ describe("Getters", () => {
     });
 
     it("should type check value argument", () => {
-      expectTypeOf(key(NumberEnum)).parameters.toEqualTypeOf<[Nullable<number>]>();
-      expectTypeOf(key(NumberEnum)).returns.toEqualTypeOf<Optional<keyof typeof NumberEnum>>();
+      expectTypeOf(E.key(NumberEnum)).parameters.toEqualTypeOf<[Nullable<number>]>();
+      expectTypeOf(E.key(NumberEnum)).returns.toEqualTypeOf<Optional<keyof typeof NumberEnum>>();
 
-      expectTypeOf(key(StringEnum)).parameters.toEqualTypeOf<[Nullable<string>]>();
-      expectTypeOf(key(StringEnum)).returns.toEqualTypeOf<Optional<keyof typeof StringEnum>>();
+      expectTypeOf(E.key(StringEnum)).parameters.toEqualTypeOf<[Nullable<string>]>();
+      expectTypeOf(E.key(StringEnum)).returns.toEqualTypeOf<Optional<keyof typeof StringEnum>>();
 
-      expectTypeOf(key(HetEnum)).parameters.toEqualTypeOf<[Nullable<number | string>]>();
-      expectTypeOf(key(HetEnum)).returns.toEqualTypeOf<Optional<keyof typeof HetEnum>>();
+      expectTypeOf(E.key(HetEnum)).parameters.toEqualTypeOf<[Nullable<number | string>]>();
+      expectTypeOf(E.key(HetEnum)).returns.toEqualTypeOf<Optional<keyof typeof HetEnum>>();
     });
   });
 });
 
 describe("Predicates", () => {
   it("should check key", () => {
-    expect(isKey(HetEnum, "A")).toBe(true);
-    expect(isKey(HetEnum, "0")).toBe(false);
+    expect(E.isKey(HetEnum, "A")).toBe(true);
+    expect(E.isKey(HetEnum, "0")).toBe(false);
 
-    expect(isKey(HetEnum, "B")).toBe(true);
+    expect(E.isKey(HetEnum, "B")).toBe(true);
 
-    expect(isKey(HetEnum, "C")).toBe(true);
-    expect(isKey(HetEnum, "0.1")).toBe(false);
+    expect(E.isKey(HetEnum, "C")).toBe(true);
+    expect(E.isKey(HetEnum, "0.1")).toBe(false);
 
-    expect(isKey(HetEnum, "D")).toBe(true);
-    expect(isKey(HetEnum, "E")).toBe(false);
+    expect(E.isKey(HetEnum, "D")).toBe(true);
+    expect(E.isKey(HetEnum, "E")).toBe(false);
   });
 
   it("should check value", () => {
-    expect(isValue(HetEnum, "A")).toBe(false);
-    expect(isValue(HetEnum, 0)).toBe(true);
+    expect(E.isValue(HetEnum, "A")).toBe(false);
+    expect(E.isValue(HetEnum, 0)).toBe(true);
 
-    expect(isValue(HetEnum, "B")).toBe(true);
+    expect(E.isValue(HetEnum, "B")).toBe(true);
 
-    expect(isValue(HetEnum, "C")).toBe(false);
-    expect(isValue(HetEnum, 0.1)).toBe(true);
+    expect(E.isValue(HetEnum, "C")).toBe(false);
+    expect(E.isValue(HetEnum, 0.1)).toBe(true);
 
-    expect(isValue(HetEnum, "D")).toBe(false);
-    expect(isValue(HetEnum, "E")).toBe(true);
+    expect(E.isValue(HetEnum, "D")).toBe(false);
+    expect(E.isValue(HetEnum, "E")).toBe(true);
   });
 
   it("should type check value", () => {
-    expect(isValue(NumberEnum, 1)).toBe(true);
+    expect(E.isValue(NumberEnum, 1)).toBe(true);
     // @ts-expect-error: only number values are allowed
-    expect(isValue(NumberEnum, "A")).toBe(false);
+    expect(E.isValue(NumberEnum, "A")).toBe(false);
 
-    expect(isValue(StringEnum, "A")).toBe(true);
+    expect(E.isValue(StringEnum, "A")).toBe(true);
     // @ts-expect-error: only string values are allowed
-    expect(isValue(StringEnum, 0)).toBe(false);
+    expect(E.isValue(StringEnum, 0)).toBe(false);
 
-    expect(isValue(HetEnum, 0)).toBe(true);
-    expect(isValue(HetEnum, "B")).toBe(true);
+    expect(E.isValue(HetEnum, 0)).toBe(true);
+    expect(E.isValue(HetEnum, "B")).toBe(true);
     // @ts-expect-error: only string or number values are allowed
-    expect(isValue(HetEnum, true)).toBe(false);
+    expect(E.isValue(HetEnum, true)).toBe(false);
   });
 
   describe("Curried", () => {
     it("should check key", () => {
-      const isEnumKey = isKey(HetEnum);
+      const isEnumKey = E.isKey(HetEnum);
 
       expect(isEnumKey("A")).toBe(true);
       expect(isEnumKey("0")).toBe(false);
@@ -222,7 +224,7 @@ describe("Predicates", () => {
     });
 
     it("should check value", () => {
-      const isEnumValue = isValue(HetEnum);
+      const isEnumValue = E.isValue(HetEnum);
 
       expect(isEnumValue("A")).toBe(false);
       expect(isEnumValue(0)).toBe(true);
@@ -237,14 +239,14 @@ describe("Predicates", () => {
     });
 
     it("should type check value", () => {
-      expectTypeOf(isValue(NumberEnum)).parameters.toEqualTypeOf<[Nullable<number>]>();
-      expectTypeOf(isValue(NumberEnum)).returns.toEqualTypeOf<boolean>();
+      expectTypeOf(E.isValue(NumberEnum)).parameters.toEqualTypeOf<[Nullable<number>]>();
+      expectTypeOf(E.isValue(NumberEnum)).returns.toEqualTypeOf<boolean>();
 
-      expectTypeOf(isValue(StringEnum)).parameters.toEqualTypeOf<[Nullable<string>]>();
-      expectTypeOf(isValue(StringEnum)).returns.toEqualTypeOf<boolean>();
+      expectTypeOf(E.isValue(StringEnum)).parameters.toEqualTypeOf<[Nullable<string>]>();
+      expectTypeOf(E.isValue(StringEnum)).returns.toEqualTypeOf<boolean>();
 
-      expectTypeOf(isValue(HetEnum)).parameters.toEqualTypeOf<[Nullable<number | string>]>();
-      expectTypeOf(isValue(HetEnum)).returns.toEqualTypeOf<boolean>();
+      expectTypeOf(E.isValue(HetEnum)).parameters.toEqualTypeOf<[Nullable<number | string>]>();
+      expectTypeOf(E.isValue(HetEnum)).returns.toEqualTypeOf<boolean>();
     });
   });
 });
@@ -253,7 +255,7 @@ describe("Iteration", () => {
   it("should iterate over entries", () => {
     const entries: [string, number | string][] = [];
 
-    forEach(HetEnum, (value, key, enumObj) => {
+    E.forEach(HetEnum, (value, key, enumObj) => {
       expectTypeOf(value).toEqualTypeOf<HetEnum>();
       expectTypeOf(key).toEqualTypeOf<keyof typeof HetEnum>();
       expectTypeOf(enumObj).toEqualTypeOf<typeof HetEnum>();
@@ -271,7 +273,7 @@ describe("Iteration", () => {
 
   describe("Curried with iteratee last", () => {
     it("should iterate over entries", () => {
-      const forEachEnum = forEach(HetEnum);
+      const forEachEnum = E.forEach(HetEnum);
       expectTypeOf(forEachEnum).parameters.toEqualTypeOf<[EnumIteratee<typeof HetEnum>]>();
 
       const entries: [string, number | string][] = [];
@@ -297,14 +299,14 @@ describe("Iteration", () => {
     it("should iterate over entries", () => {
       const entries: [string, number | string][] = [];
 
-      const pushEntries = forEach<typeof HetEnum>((value, key, enumObj) => {
-        expectTypeOf(value).toEqualTypeOf<HetEnum>();
-        expectTypeOf(key).toEqualTypeOf<keyof typeof HetEnum>();
-        expectTypeOf(enumObj).toEqualTypeOf<typeof HetEnum>();
+      const pushEntries = E.forEach((value, key, enumObj) => {
+        expectTypeOf(value).toEqualTypeOf<AnyEnumValue>();
+        expectTypeOf(key).toEqualTypeOf<string>();
+        expectTypeOf(enumObj).toEqualTypeOf<AnyEnumObject>();
 
         entries.push([key, value]);
       });
-      expectTypeOf(pushEntries).parameters.toEqualTypeOf<[typeof HetEnum]>();
+      expectTypeOf(pushEntries).parameters.toEqualTypeOf<[AnyEnumObject]>();
 
       pushEntries(HetEnum);
 
