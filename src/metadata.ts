b95ce66f6ad1e453b5162160ea8497899a97657c
exuanbo <exuanbo@protonmail.com>
2024-08-22 12:28:49 +0800

    refactor: simplify metadata

--- src/metadata.ts
+++ src/metadata.ts
@@ -1,9 +1,9 @@
 import type { AnyEnumObject, AnyEnumValue, EnumKey, EnumObject } from "./types";
 
 interface EnumMetadata<V extends AnyEnumValue, T extends EnumObject<T, V>> {
-  keys?: ReadonlyArray<EnumKey<T>>;
-  keysByValue?: ReadonlyMap<V, EnumKey<T>>;
-  valuesByKey?: ReadonlyMap<EnumKey<T>, V>;
+  ks?: ReadonlyArray<EnumKey<T>>;
+  vk?: ReadonlyMap<V, EnumKey<T>>;
+  kv?: ReadonlyMap<EnumKey<T>, V>;
 }
 
 const metadataCache = /*#__PURE__*/ new WeakMap<AnyEnumObject, EnumMetadata<any, any>>();
@@ -20,33 +20,25 @@ function getMetadata<V extends AnyEnumValue, T extends EnumObject<T, V>>(
 }
 
 export function getKeys<T extends AnyEnumObject>(enumObj: T): ReadonlyArray<EnumKey<T>> {
-  const metadata = getMetadata(enumObj);
-  if (!metadata.keys) {
-    metadata.keys = <EnumKey<T>[]>(
-      Object.getOwnPropertyNames(enumObj).filter(
-        (key) => isNaN(Number(key)) && {}.propertyIsEnumerable.call(enumObj, key),
-      )
-    );
-  }
-  return metadata.keys;
+  return (getMetadata(enumObj).ks ||= <EnumKey<T>[]>(
+    Object.getOwnPropertyNames(enumObj).filter(
+      (key) => isNaN(Number(key)) && {}.propertyIsEnumerable.call(enumObj, key),
+    )
+  ));
 }
 
 export function getKeysByValue<V extends AnyEnumValue, T extends EnumObject<T, V>>(
   enumObj: T,
 ): ReadonlyMap<V, EnumKey<T>> {
-  const metadata = getMetadata<V, T>(enumObj);
-  if (!metadata.keysByValue) {
-    metadata.keysByValue = new Map(getKeys(enumObj).map((key) => [enumObj[key], key]));
-  }
-  return metadata.keysByValue;
+  return (getMetadata<V, T>(enumObj).vk ||= new Map(
+    getKeys(enumObj).map((key) => [enumObj[key], key]),
+  ));
 }
 
 export function getValuesByKey<V extends AnyEnumValue, T extends EnumObject<T, V>>(
   enumObj: T,
 ): ReadonlyMap<EnumKey<T>, V> {
-  const metadata = getMetadata<V, T>(enumObj);
-  if (!metadata.valuesByKey) {
-    metadata.valuesByKey = new Map(getKeys(enumObj).map((key) => [key, enumObj[key]]));
-  }
-  return metadata.valuesByKey;
+  return (getMetadata<V, T>(enumObj).kv ||= new Map(
+    getKeys(enumObj).map((key) => [key, enumObj[key]]),
+  ));
 }
