430d472ff01f7e998f8c4acc151207ea4cd260ed
Xuanbo Cheng <exuanbo@protonmail.com>
2024-08-22 08:51:33 +0100

    refactor: simplify metadata (#4)
    * refactor: simplify metadata
    
    * perf: use `Object.keys()`
    
    * refactor: store entries in metadata

--- src/metadata.ts
+++ src/metadata.ts
@@ -1,9 +1,9 @@
-import type { AnyEnumObject, AnyEnumValue, EnumKey, EnumObject } from "./types";
+import type { AnyEnumObject, AnyEnumValue, EnumEntry, EnumKey, EnumObject } from "./types";
 
 interface EnumMetadata<V extends AnyEnumValue, T extends EnumObject<T, V>> {
-  keys?: ReadonlyArray<EnumKey<T>>;
-  keysByValue?: ReadonlyMap<V, EnumKey<T>>;
-  valuesByKey?: ReadonlyMap<EnumKey<T>, V>;
+  ks?: ReadonlyArray<EnumKey<T>>;
+  kvs?: ReadonlyArray<EnumEntry<T>>;
+  v_k?: ReadonlyMap<V, EnumKey<T>>;
 }
 
 const metadataCache = /*#__PURE__*/ new WeakMap<AnyEnumObject, EnumMetadata<any, any>>();
@@ -20,33 +20,21 @@ function getMetadata<V extends AnyEnumValue, T extends EnumObject<T, V>>(
 }
 
 export function getKeys<T extends AnyEnumObject>(enumObj: T): ReadonlyArray<EnumKey<T>> {
-  const metadata = getMetadata(enumObj);
-  if (!metadata.keys) {
-    metadata.keys = <EnumKey<T>[]>(
-      Object.getOwnPropertyNames(enumObj).filter(
-        (key) => isNaN(Number(key)) && {}.propertyIsEnumerable.call(enumObj, key),
-      )
-    );
-  }
-  return metadata.keys;
+  return (getMetadata(enumObj).ks ||= <EnumKey<T>[]>(
+    Object.keys(enumObj).filter((key) => isNaN(Number(key)))
+  ));
 }
 
-export function getKeysByValue<V extends AnyEnumValue, T extends EnumObject<T, V>>(
+export function getEntries<V extends AnyEnumValue, T extends EnumObject<T, V>>(
+  enumObj: T,
+): ReadonlyArray<EnumEntry<T>> {
+  return (getMetadata<V, T>(enumObj).kvs ||= getKeys(enumObj).map((key) => [key, enumObj[key]]));
+}
+
+export function getValueKeyMap<V extends AnyEnumValue, T extends EnumObject<T, V>>(
   enumObj: T,
 ): ReadonlyMap<V, EnumKey<T>> {
-  const metadata = getMetadata<V, T>(enumObj);
-  if (!metadata.keysByValue) {
-    metadata.keysByValue = new Map(getKeys(enumObj).map((key) => [enumObj[key], key]));
-  }
-  return metadata.keysByValue;
-}
-
-export function getValuesByKey<V extends AnyEnumValue, T extends EnumObject<T, V>>(
-  enumObj: T,
-): ReadonlyMap<EnumKey<T>, V> {
-  const metadata = getMetadata<V, T>(enumObj);
-  if (!metadata.valuesByKey) {
-    metadata.valuesByKey = new Map(getKeys(enumObj).map((key) => [key, enumObj[key]]));
-  }
-  return metadata.valuesByKey;
+  return (getMetadata<V, T>(enumObj).v_k ||= new Map(
+    getKeys(enumObj).map((key) => [enumObj[key], key]),
+  ));
 }
